cdbe8adf15a9ccc5604da715f8e3fcfea066d995
JEdilrey <joshedilreydeguzman@gmail.com>
2026-03-04 17:24:56 +0800

    fix: add lazy loading to emotion detector

--- spoofing_detection_api/app/models/emotion_detection.py
+++ spoofing_detection_api/app/models/emotion_detection.py
@@ -9,9 +9,17 @@ class EmotionDetection:
     def __init__(self):
         pass
 
+    def load_model(self):
+        try:
+            self.model = FaceAnalysis(model_name='emotion')
+        except Exception as e:
+            print(f"Error loading emotion detection model: {e}")
+            self.model = None
+
     def detect(self, image: Image) -> str | int | None:
         try:
             img_array = np.array(image)
+
             analysis = FaceAnalysis(model_name='emotion').predict(img_array)
             return analysis
         except Exception as e:
